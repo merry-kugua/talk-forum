6b90763fdb4bb901a40b0461b7327e215c04a16d
宇 <yu@yudeMacBook-Pro.local>
2022-03-19 23:35:56 +0800

    配置路由 Header组件信息,添加normalize样式统一化

---
+++ src/components/HeaderHome.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>1111</div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+module.exports = {
+  // 基本路径
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  // 输出文件目录
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+  // 是否开启eslint保存检测，有效值：ture | false | 'error'
+  lintOnSave: true,
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false,
+  /**
+   * webpack配置,see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+   **/
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+    svgRule.uses.clear();
+    svgRule
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: "icon-[name]",
+        include: ["./src/icons"]
+      });
+    config.optimization.runtimeChunk('single'),
+    {
+      from: path.resolve(__dirname, './public/robots.txt'), //防爬虫文件
+      to: './' //到根目录下
+    }
+  },
+  configureWebpack: (config) => {
+    // 配置解析别名
+    config.resolve = {
+      extensions: ['.js', '.json', '.vue'],//自动添加文件后缀名
+      alias: {
+        'vue': 'vue/dist/vue.js',
+        '@': path.resolve(__dirname, './src'),
+        '@c': path.resolve(__dirname, './src/components'),
+      }
+    }
+  },
+  // css相关配置
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: true,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {
+      sass: {
+        prependData: `@import "./src/styles/main.scss";`
+      }
+    },
+    // 启用 CSS modules for all css / pre-processor files.
+    requireModuleExtension: true
+  },
+  // use thread-loader for babel & TS in production build
+  // enabled by default if the machine has more than 1 cores
+  parallel: require('os').cpus().length > 1,
+  /**
+   *  PWA 插件相关配置,see https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
+   */
+  pwa: {},
+  // webpack-dev-server 相关配置
+  devServer: {
+    open: false, // 编译完成是否打开网页
+    host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
+    port: 8080, // 访问端口
+    https: false, // 编译失败时刷新页面
+    hot: true, // 开启热加载
+    hotOnly: false,
+    proxy: null, // 设置代理
+    proxy: {
+      '/devApi': {
+        target: "localhost", // target处填写项目开发过程中的实际地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/devApi': ''
+        }
+      }
+    },
+    overlay: { // 全屏模式下是否显示脚本错误
+      warnings: true,
+      errors: true
+    },
+    before: app => {
+    }
+  },
+  /**
+   * 第三方插件配置
+   */
+  pluginOptions: {}
+}
